5f7a4feb84a0ace616aba0a3f0dded469cff6821
Daniil <zelenko.danil@gmail.com>
2024-09-01 15:37:04 +0200

    refresh db and fork entity manager for request

--- src/plugin/mikroorm.ts
+++ src/plugin/mikroorm.ts
@@ -1,10 +1,15 @@
 import fp from 'fastify-plugin';
-import { MikroORM } from '@mikro-orm/core';
+import { MikroORM, RequestContext } from '@mikro-orm/core';
 import mikroOrmConfig from '../mikro-orm.config.js';
 import { FastifyInstance } from 'fastify';
 
 async function mikroOrmPlugin(fastify: FastifyInstance) {
   const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.schema.refreshDatabase();
+  fastify.addHook('onRequest', (request, reply, done) => {
+    RequestContext.create(orm.em, done);
+  });
+
   fastify.decorate('orm', orm);
 
   fastify.addHook('onClose', async () => {
