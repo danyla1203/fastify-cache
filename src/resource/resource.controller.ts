0417e3bd1660f28ca44fe120beb6803f92618dbc
Daniil <zelenko.danil@gmail.com>
2024-09-01 18:30:51 +0200

    Delete, update

--- src/resource/resource.controller.ts
+++ src/resource/resource.controller.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance, FastifyRequest } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { Resource } from './resource.entity.js';
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 
@@ -16,7 +16,6 @@ export const resourceController: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify: FastifyInstance,
 ) => {
   const em = fastify.orm.em;
-
   fastify.route({
     url: '/',
     method: 'GET',
@@ -40,7 +39,7 @@ export const resourceController: FastifyPluginAsyncJsonSchemaToTs = async (
     schema: {
       body: {
         type: 'object',
-        required: ['name'],
+        required: ['name', 'description', 'text', 'price'],
         properties: {
           name: { type: 'string' },
           description: { type: 'string' },
@@ -61,6 +60,74 @@ export const resourceController: FastifyPluginAsyncJsonSchemaToTs = async (
     method: 'GET',
     handler: async () => {
       return em.findAll(Resource);
+    },
+  });
+
+  fastify.route({
+    url: '/',
+    method: 'DELETE',
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          id: { type: 'number' },
+        },
+        required: ['id'],
+      },
+    },
+    handler: async (
+      { query }: FastifyRequest<{ Querystring: IQuerystring }>,
+      reply: FastifyReply,
+    ) => {
+      const toDelete = await em.findOne(Resource, query.id);
+      return !toDelete
+        ? reply.code(404).send('Resource not found')
+        : em.removeAndFlush(toDelete);
+    },
+  });
+
+  fastify.route({
+    url: '/',
+    method: 'PATCH',
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          id: { type: 'number' },
+        },
+        required: ['id'],
+      },
+      body: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          description: { type: 'string' },
+          text: { type: 'string' },
+          price: { type: 'number' },
+        },
+      },
+    },
+    handler: async (
+      {
+        body,
+        query,
+      }: FastifyRequest<{
+        Body: Partial<InsertResourceDto>;
+        Querystring: IQuerystring;
+      }>,
+      reply: FastifyReply,
+    ) => {
+      const toUpdate = await em.findOne(Resource, query.id);
+      if (!toUpdate) {
+        reply.code(404).send('Resource not found');
+        return null;
+      }
+      toUpdate.name = body.name || toUpdate.name;
+      toUpdate.price = body.price || toUpdate.price;
+      toUpdate.description = body.description || toUpdate.description;
+      toUpdate.text = body.text || toUpdate.text;
+      await em.flush();
+      return toUpdate;
     },
   });
 };
